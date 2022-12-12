c0eddecb21c4d39f3aa2ce4f314e40c083b55729
BenjaminIrwin <benjamintdirwin@gmail.com>
2022-12-12 12:09:54 +0000

    First changes for inpainting

--- trainer/diffusers_trainer.py
+++ trainer/diffusers_trainer.py
@@ -1,10 +1,3 @@
-# Install bitsandbytes:
-# `nvcc --version` to get CUDA version.
-# `pip install -i https://test.pypi.org/simple/ bitsandbytes-cudaXXX` to install for current CUDA.
-# Example Usage:
-# Single GPU: torchrun --nproc_per_node=1 trainer/diffusers_trainer.py --model="CompVis/stable-diffusion-v1-4" --run_name="liminal" --dataset="liminal-dataset" --hf_token="hf_blablabla" --bucket_side_min=64 --use_8bit_adam=True --gradient_checkpointing=True --batch_size=1 --fp16=True --image_log_steps=250 --epochs=20 --resolution=768 --use_ema=True
-# Multiple GPUs: torchrun --nproc_per_node=N trainer/diffusers_trainer.py --model="CompVis/stable-diffusion-v1-4" --run_name="liminal" --dataset="liminal-dataset" --hf_token="hf_blablabla" --bucket_side_min=64 --use_8bit_adam=True --gradient_checkpointing=True --batch_size=10 --fp16=True --image_log_steps=250 --epochs=20 --resolution=768 --use_ema=True
-
 import argparse
 import socket
 import torch
@@ -138,7 +131,7 @@ def generate_masked_image(image, mask):
     return image * (mask < 0.5)
 
 
-def binary_mask_to_tensor(image: Image.Image) -> torch.Tensor:
+def binary_mask_to_tensor(image) -> torch.Tensor:
     # Convert the PIL image to a NumPy array
     np_image = np.array(image)
 
@@ -218,7 +211,7 @@ class Validation():
                 return False
             print(f'WARNING: Image cannot be opened: {error}')
             return False
-        except:
+        except shutil.Error as error:
             print(f'WARNING: Image cannot be opened: {error}')
             return False
 
