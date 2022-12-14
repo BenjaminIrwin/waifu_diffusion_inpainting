8f626e4f1da0af18d27bd875d42a4dc4b5b5f3fb
BenjaminIrwin <benjamintdirwin@gmail.com>
2022-12-14 09:12:13 +0000

    debug print out

--- trainer/diffusers_trainer.py
+++ trainer/diffusers_trainer.py
@@ -407,6 +407,9 @@ class InpaintDataset(torch.utils.data.Dataset):
         torch.set_rng_state(state)
         masked_image = generate_masked_image(return_dict['image_pixel_values'], return_dict['mask_pixel_values'])
 
+        print('SHAPE OF MASKED IMAGE BEFORE TRANSFORMATIONS: ' + str(masked_image.shape))
+
+
         return_dict['masked_image_pixel_values'] = self.transforms(masked_image.cpu().numpy()).to(self.device)
 
         # TODO: Do we still need this?
