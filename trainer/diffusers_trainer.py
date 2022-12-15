e7c991d54857785ee97257b1f8c0a45338b474d5
BenjaminIrwin <benjamintdirwin@gmail.com>
2022-12-15 17:34:14 +0000

    debug printout

--- trainer/diffusers_trainer.py
+++ trainer/diffusers_trainer.py
@@ -417,6 +417,7 @@ class InpaintDataset(torch.utils.data.Dataset):
         pixel_values = [example["image_pixel_values"] for example in examples]
         mask_values = [example["mask_pixel_values"] for example in examples]
         masked_image_values = [example["masked_image_pixel_values"] for example in examples]
+        names = [example["input_ids"] for example in examples]
 
         pixel_values = torch.stack(pixel_values).to(memory_format=torch.contiguous_format).float()
         mask_values = torch.stack(mask_values).to(memory_format=torch.contiguous_format).float()
@@ -498,6 +499,7 @@ class InpaintDataset(torch.utils.data.Dataset):
         input_ids = torch.stack(tuple(input_ids))
 
         batch = {
+            'names': names,
             "input_ids": input_ids,
             'tokens': tokens,
             "image_pixel_values": pixel_values,
@@ -815,7 +817,7 @@ def main():
                         progress_bar.update(1)
                     global_step += 1
                     continue
-
+                print(batch['names'])
                 b_start = time.perf_counter()
                 # TODO: do we need with_torch_no_grad here?
                 with torch.no_grad():
